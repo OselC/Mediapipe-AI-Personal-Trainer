775b98d5138a6f4237a4f05fb74663ea0807e04c
Osel Citta C <osel.citta.chen@gmail.com>
2026-06-30 00:07:53 +0700

    Add Python files

---
+++ AITrainer.py
@@ -0,0 +1,69 @@
+import cv2
+import numpy as np
+import time
+import PoseModule as pm
+
+cap = cv2.VideoCapture(0)
+detector = pm.poseDetector()
+
+count = 0
+dir = 0
+pTime = 0
+
+while True:
+    success, img = cap.read()
+    img = cv2.resize(img, (1280, 720))
+    # img = cv2.imread("Training Videos/test.jpg")
+    img = detector.findPose(img, False)
+    lmList = detector.findPosition(img, False)
+
+    if len(lmList) != 0:
+        # # Right arm
+        # detector.findAngle(img, 12, 14, 16)
+        # Left arm
+        angle = detector.findAngle(img, 11, 13, 15)
+        per = np.interp(angle, (210, 310), (0, 100))
+        bar = np.interp(angle, (210, 310), (650, 100))
+        # print(angle, per)
+
+        # Check for the dumbell curls
+        color = (255, 0, 255)
+        if per == 100:
+            color = (0, 255, 0)
+            if dir == 0:
+                count += 0.5
+                dir = 1
+                
+        if per == 0:
+            color = (0, 255, 0)
+            if dir == 1:
+                count += 0.5
+                dir = 0
+
+        print(count)
+    
+    # Bar
+    cv2.rectangle(img, (1100, 100), (1175, 650), color, 3)
+    cv2.rectangle(img, (1100, int(bar)), (1175, 650), color, cv2.FILLED)
+    cv2.putText(img, f"{int(per)}%", (1100, 75), 
+                    cv2.FONT_HERSHEY_PLAIN, 3, color, 3)
+
+    # Curl count
+    cv2.rectangle(img, (30, 450), (170, 620), (255, 255, 255), cv2.FILLED)
+    cv2.putText(img, str(int(count)), (28, 620), 
+                    cv2.FONT_HERSHEY_PLAIN, 15, (0, 0, 0), 15)
+    
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+
+    cv2.putText(img, f"FPS: {int(fps)}", (40, 70), 
+                    cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
+
+    cv2.imshow("Image", img)
+
+    if cv2.waitKey(1) & 0xFF == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
